251fadc68b5c315456ca1b2c7c248211575b82e7
Sanjeeb555 <sanjeeb4223@gmail.com>
2026-01-06 18:58:12 +0530

    components

---
+++ src/components/Dp.js
@@ -0,0 +1,10 @@
+import React from "react"
+import image from "./Dp.jpeg"
+
+export default function Dp(){
+    return (
+        <div className="dpimage">
+            <img src={image} className="dpimage"/>
+        </div>
+    )
+}
